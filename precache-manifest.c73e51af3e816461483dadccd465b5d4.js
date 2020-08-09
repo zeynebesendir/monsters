effde69d7a29a6c749abc2a05c3e672762a7386a
Zeyneb Esendir <zeynebesendir@gmail.com>
2020-08-09 17:56:02 +0100

    Updates

---
+++ precache-manifest.c73e51af3e816461483dadccd465b5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2ee2709e1de1c96da8d04b50e6aaa4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "4b194a276089bc5d5d51",
+    "url": "/monsters/static/css/main.26f69440.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b194a276089bc5d5d51",
+    "url": "/monsters/static/js/main.072e3ad4.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
